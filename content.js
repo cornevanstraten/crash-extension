b3e408e65d81216ac605b8b26206fbc2d04f5ae6
Corné van Straten <vanstratenc@gmail.com>
2020-04-08 23:09:07 -0400

    remove logging

--- content.js
+++ content.js
@@ -4,7 +4,6 @@ function gotMessage(message, sender, sendResponse) {
   if (message.txt === "req URL") {
     const currentURL = window.location.href;
     sendResponse({ url: currentURL });
-    console.log(currentURL);
   } else {
     window.location.href = message.redirect;
   }
